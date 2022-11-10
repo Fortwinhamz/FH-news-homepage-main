f7b8912e7847d2a47f4abed6c725e6bba631cd5c
Fortwin <basirathamz@gmail.com>
2022-11-10 14:04:57 +0100

    Add files via upload

---
+++ news-homepage-main/main.js
@@ -0,0 +1,20 @@
+const fortwin = {
+    firstName: 'Baseeroh',
+    lastName: ' Hamzat',
+    age: 19,
+    hobbies: ['music' , 'movies' , 'football' , 'coding'],
+    address:{
+        street: '21, Prince Omotubora',
+        city: 'Ikorodu',
+        state: 'Lagos'
+    }
+
+}
+
+console.log(fortwin)
+
+// console.log(fortwin.hobbies)
+console.log(fortwin.firstName,fortwin.lastName)
+// console.log(fortwin.address)
+// console.log(fortwin.address.state)
+// console.log(fortwin.address.city)
